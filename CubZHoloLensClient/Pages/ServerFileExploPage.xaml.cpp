997e395364c8f7c240d4fe268d02e0dce5785e74
Louis Ventre <louis.ventre@epitech.eu>
2018-07-02 21:52:52 +0200

    Add Oihan work on ServerFileExploPage

--- CubZHoloLensClient/Pages/ServerFileExploPage.xaml.cpp
+++ CubZHoloLensClient/Pages/ServerFileExploPage.xaml.cpp
@@ -31,6 +31,7 @@ ServerFileExploPage::ServerFileExploPage()
 	Windows::UI::Xaml::DataTemplate ^fTemplate = static_cast<DataTemplate^>(this->Resources->Lookup("FileTemplate"));
 
 	this->FileView->ItemTemplateSelector = ref new FileDataTemplateSelector(fTemplate, dTemplate);
+	this->path = ".";
 }
 
 void CubZHoloLensClient::ServerFileExploPage::OnListFile(Windows::Foundation::Collections::IVector<Platform::String^>^ fileList)
@@ -39,7 +40,7 @@ void CubZHoloLensClient::ServerFileExploPage::OnListFile(Windows::Foundation::Co
 	/*TRACE("Got files:" << std::endl);*/
 	for (Platform::String ^fileName : fileList)
 	{
-		files->Append(ref new File(fileName, FileType::FILE));
+		files->Append(ref new File(fileName, FileType::DIRECTORY));
 		/*TRACE("Name: " << Utility::platformStringToString(fileName) << std::endl);*/
 	}
 
@@ -52,15 +53,14 @@ void CubZHoloLensClient::ServerFileExploPage::OnListFile(Windows::Foundation::Co
 void CubZHoloLensClient::ServerFileExploPage::Button_GoBack(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
 {
 	//Unregister for event
-	//CubZHoloLensClient::HoloLensContext::Instance()->getTCPClient()->ListFileEvent -= _listFileToken;
-	//_threadPoolTimerListFiles->Cancel();
-	//delete _threadPoolTimerListFiles;
+	CubZHoloLensClient::HoloLensContext::Instance()->getTCPClient()->ListFileEvent -= _listFileToken;
+	_threadPoolTimerListFiles->Cancel();
+	delete _threadPoolTimerListFiles;
 
 	this->Dispatcher->RunAsync(Windows::UI::Core::CoreDispatcherPriority::Normal, ref new Windows::UI::Core::DispatchedHandler([this]() {
 		this->Frame->GoBack();
 	}));
 }
-
 
 void CubZHoloLensClient::ServerFileExploPage::FileView_SelectionChanged(Platform::Object^ sender, Windows::UI::Xaml::Controls::SelectionChangedEventArgs^ e)
 {
@@ -68,20 +68,55 @@ void CubZHoloLensClient::ServerFileExploPage::FileView_SelectionChanged(Platform
 
 void CubZHoloLensClient::ServerFileExploPage::OnNavigatedTo(Windows::UI::Xaml::Navigation::NavigationEventArgs ^ e)
 {
-	////Register to receive info on fileList
-	//_listFileToken = CubZHoloLensClient::HoloLensContext::Instance()->getTCPClient()->ListFileEvent += ref new CubZHoloLensClient::WinNetwork::FileListEvent(this,
-	//	&CubZHoloLensClient::ServerFileExploPage::OnListFile);
+	//Register to receive info on fileList
+	_listFileToken = CubZHoloLensClient::HoloLensContext::Instance()->getTCPClient()->ListFileEvent += ref new CubZHoloLensClient::WinNetwork::FileListEvent(this,
+		&CubZHoloLensClient::ServerFileExploPage::OnListFile);
 
-	////Update file scene every 5 seconds
-	//_timeSpanListFile.Duration = 50000000;
+	//Update file scene every 5 seconds
+	_timeSpanListFile.Duration = 50000000;
 
-	//_threadPoolTimerListFiles = Windows::System::Threading::ThreadPoolTimer::CreatePeriodicTimer(
-	//	ref new Windows::System::Threading::TimerElapsedHandler([](Windows::System::Threading::ThreadPoolTimer^ source)
-	//{
-	//	CubZHoloLensClient::HoloLensContext::Instance()->getTCPClient()->listServerFiles(".");
-	//}), _timeSpanListFile);
+	_threadPoolTimerListFiles = Windows::System::Threading::ThreadPoolTimer::CreatePeriodicTimer(
+		ref new Windows::System::Threading::TimerElapsedHandler([&, this](Windows::System::Threading::ThreadPoolTimer^ source)
+	{
+		CubZHoloLensClient::HoloLensContext::Instance()->getTCPClient()->listServerFiles(this->path);
+	}), _timeSpanListFile);
 
 
 	TRACE("Navigated to ServerFileExplo" << std::endl);
-	CubZHoloLensClient::HoloLensContext::Instance()->getTCPClient()->listServerFiles(".");
+	CubZHoloLensClient::HoloLensContext::Instance()->getTCPClient()->listServerFiles(this->path);
+}
+
+
+void CubZHoloLensClient::ServerFileExploPage::GoInsideButton_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	//if c'est un fichier alors ne rien faire
+
+	//sinon faire le reste
+	this->path = this->path + "/" + this->FileName->Text;
+}
+
+
+void CubZHoloLensClient::ServerFileExploPage::GoPrevFolder_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	if (this->path != ".") {
+		std::string newpath;
+		newpath = Utility::platformStringToString(this->path);
+		std::vector<std::string> array;
+		std::stringstream ss(newpath);
+		std::string tmp;
+		while (std::getline(ss, tmp, '/'))
+		{
+			array.push_back(tmp);
+		}
+		newpath = "";
+		array.pop_back();
+		for (auto it = array.begin(); it != array.end(); ++it)
+		{
+			if (it != array.end()) {
+				newpath = newpath + (*it) + "/";
+			}
+		}
+		newpath = newpath.substr(0, newpath.size() - 1);
+		this->path = Utility::stringToPlatformString(newpath);
+	}
 }
